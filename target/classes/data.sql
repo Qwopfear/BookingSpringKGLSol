cd4aeafb8511d326565ddbea82da00d11cd3fd0b
Qwopfear <seva552.seva@gmail.com>
2022-09-12 19:14:13 +0100

    Target added

---
+++ target/classes/data.sql
@@ -0,0 +1,46 @@
+CREATE  TABLE Client (
+     id long auto_increment primary key ,
+     name varchar(30)
+);
+
+CREATE  TABLE Rentable (
+    id long auto_increment primary key ,
+    name varchar(30),
+    description varchar(225),
+    price long,
+    area long
+);
+
+
+CREATE  TABLE Reservation (
+    id long auto_increment,
+    client_id long,
+    rent_id long,
+    rent_Start DATE,
+    rent_End DATE
+);
+ALTER TABLE Reservation
+    ADD FOREIGN KEY (client_id)
+        REFERENCES Client(id);
+
+ALTER TABLE Reservation
+    ADD FOREIGN KEY (rent_id)
+        REFERENCES Rentable(id);
+
+
+
+INSERT INTO Rentable (name, description, price, area)VALUES
+('Penthouse','Butifull appartment whith good atractions',1000,100),
+('House around lake','Natural atraction around wild lake',1000,100);
+
+
+INSERT INTO Client (name)
+VALUES ('SEVA'), ('Diana');
+
+
+INSERT INTO Reservation (client_id, rent_id,rent_Start,rent_End)VALUES
+                                                                  ( 1,1,'2022-12-12','2023-01-01' ),
+                                                                  ( 1,2,'2023-01-02','2023-02-28'),
+                                                                  ( 2,2,'2022-12-01','2022-12-11' ),
+                                                                  ( 2,1,'2023-01-02','2023-02-28' );
+
